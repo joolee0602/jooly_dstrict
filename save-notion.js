16afeaba1e4451bea71d530754535f05a280cc12
joolee0602 <joo.lee0602@gmail.com>
2026-03-23 15:56:14 +0900

    Add files via upload

---
+++ save-notion.js
@@ -0,0 +1,50 @@
+exports.handler = async function(event, context) {
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
+      },
+      body: ''
+    };
+  }
+
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const NOTION_TOKEN = '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';
+
+  try {
+    const { pageId, children } = JSON.parse(event.body);
+
+    const response = await fetch('https://api.notion.com/v1/blocks/' + pageId + '/children', {
+      method: 'PATCH',
+      headers: {
+        'Authorization': 'Bearer ' + NOTION_TOKEN,
+        'Notion-Version': '2022-06-28',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ children })
+    });
+
+    const data = await response.json();
+
+    return {
+      statusCode: response.status,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: error.message })
+    };
+  }
+};
