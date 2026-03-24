b8159e0f4ec3e6642fac682654ede4a1f795a9f3
joolee0602 <joo.lee0602@gmail.com>
2026-03-24 09:52:37 +0900

    Notion 토큰을 환경변수로 변경

--- netlify/functions/save-notion.js
+++ netlify/functions/save-notion.js
@@ -2,17 +2,19 @@ exports.handler = async function(event) {
   if (event.httpMethod === 'OPTIONS') {
     return { statusCode: 200, headers: { 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': 'Content-Type', 'Access-Control-Allow-Methods': 'POST, OPTIONS' }, body: '' };
   }
-  const NOTION_TOKEN = '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';
+  if (event.httpMethod !== 'POST') return { statusCode: 405, body: 'Method Not Allowed' };
+  const NOTION_TOKEN = process.env.NOTION_TOKEN;
+  if (!NOTION_TOKEN) return { statusCode: 500, headers: { 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify({ error: 'NOTION_TOKEN not set' }) };
   try {
     const { pageId, children } = JSON.parse(event.body);
-    const res = await fetch('https://api.notion.com/v1/blocks/' + pageId + '/children', {
+    const response = await fetch('https://api.notion.com/v1/blocks/' + pageId + '/children', {
       method: 'PATCH',
       headers: { 'Authorization': 'Bearer ' + NOTION_TOKEN, 'Notion-Version': '2022-06-28', 'Content-Type': 'application/json' },
       body: JSON.stringify({ children })
     });
-    const data = await res.json();
-    return { statusCode: res.status, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }, body: JSON.stringify(data) };
-  } catch (e) {
-    return { statusCode: 500, headers: { 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify({ error: e.message }) };
+    const data = await response.json();
+    return { statusCode: response.status, headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' }, body: JSON.stringify(data) };
+  } catch (error) {
+    return { statusCode: 500, headers: { 'Access-Control-Allow-Origin': '*' }, body: JSON.stringify({ error: error.message }) };
   }
 };
